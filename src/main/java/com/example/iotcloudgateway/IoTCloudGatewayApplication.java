23aea97c474bc5592a410352479b339dd0f06b35
鲍奕刚 <yigang.bao@hekr.me>
2020-06-24 18:22:31 +0800

    加上UdpClient

--- src/main/java/com/example/iotcloudgateway/IoTCloudGatewayApplication.java
+++ src/main/java/com/example/iotcloudgateway/IoTCloudGatewayApplication.java
@@ -1,15 +1,16 @@
 package com.example.iotcloudgateway;
 
 import com.example.iotcloudgateway.client.tcp.TcpClientStarter;
+import com.example.iotcloudgateway.client.udp.UdpClientStarter;
 import com.example.iotcloudgateway.mqtt.MqttServer;
 import com.example.iotcloudgateway.server.tcp.TcpServerStarter;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class IoTCloudGatewayApplication {
-
   public static void main(String[] args) throws Exception {
     MqttServer.mqttconnection();
 //    TcpServerStarter.start();
-    TcpClientStarter.start();
+//    TcpClientStarter.start();
+    UdpClientStarter.start();
   }
 }
---
+++ src/main/java/com/example/iotcloudgateway/client/udp/UdpClientStarter.java
@@ -0,0 +1,14 @@
+package com.example.iotcloudgateway.client.udp;
+
+import org.tio.core.udp.UdpClient;
+import org.tio.core.udp.UdpClientConf;
+
+public class UdpClientStarter {
+    public static final byte[] GET_MSG = {01};
+    private static UdpClientConf udpClientConf = new UdpClientConf("192.168.1.135", 3000, 5000);
+    public static void start() {
+        UdpClient udpClient = new UdpClient(udpClientConf);
+        udpClient.start();
+        udpClient.send(GET_MSG);
+    }
+}
