23eb743cb1b755a055bb5cf8400be5aed6ac5514
王佳涛 <jiatao.wang@hekr.me>
2020-07-22 11:57:37 +0800

    合并

--- src/main/java/hekr/me/iotos/softgateway/pluginAsServer/http/testController.java
+++ src/main/java/hekr/me/iotos/softgateway/pluginAsServer/http/testController.java
@@ -3,10 +3,7 @@ package hekr.me.iotos.softgateway.pluginAsServer.http;
 import hekr.me.iotos.softgateway.common.dto.BaseResp;
 import hekr.me.iotos.softgateway.common.dto.ChargeReq;
 import hekr.me.iotos.softgateway.common.dto.EntranceReq;
-import hekr.me.iotos.softgateway.common.dto.GateControlReq;
-import hekr.me.iotos.softgateway.utils.AESUtils;
 import hekr.me.iotos.softgateway.utils.JsonUtil;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,23 +17,8 @@ import java.util.List;
 import java.util.Random;
 
 @Slf4j
-@RequestPath(value = "/")
+@RequestPath(value = "/gateway")
 public class TestController {
-
-  @SneakyThrows
-  @RequestPath(value = "/GetChannelInfo")
-  public HttpResponse getChannelInfo(HttpRequest request) {
-    //     将数据解码后
-    GateControlReq gateControlReq =
-        JsonUtil.fromBytes(
-            AESUtils.decodeRequestData(new String(request.getBody())), GateControlReq.class);
-    //    GateControlReq gateControlReq = JsonUtil.fromBytes(request.getBody(),
-    // GateControlReq.class);
-    BaseResp baseResp = new BaseResp();
-    baseResp.setResMsg("通道口：" + gateControlReq.getChannelID() + " 操作成功");
-    baseResp.setResCode(0);
-    return Resps.json(request, baseResp);
-  }
 
   @Autowired Random random;
 
