e0ebdc4fd11ac2e993eaaa77f257c316a41edff9
王佳涛 <jiatao.wang@hekr.me>
2020-07-29 15:22:26 +0800

    fix httpclient

--- src/main/java/hekr/me/iotos/softgateway/pluginAsClient/http/HttpClient.java
+++ src/main/java/hekr/me/iotos/softgateway/pluginAsClient/http/HttpClient.java
@@ -13,15 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class HttpClient {
   @Autowired ProxyConfig proxyConfig;
-  EnergyMeterReq energyMeterReq = new EnergyMeterReq();
-  Map<String, Object> energyMeter = MapUtil.objectToMap(energyMeterReq);
 
   public byte[] energyMeter(EnergyMeterReq energyMeterReq) {
-    Map<String, Object> eneryMeterCtrl = MapUtil.objectToMap(energyMeterReq);
+    Map<String, Object> energyMeterCtrl = MapUtil.objectToMap(energyMeterReq);
     Map<String, String> headerParams = new HashMap<>();
     headerParams.put("Content-Type", "application/json");
     String url = proxyConfig.getHTTP_URL() + "/api/bi/GetEnergyMeterData";
     HttpUtils httpUtils = new HttpUtils();
-    return httpUtils.get(url, headerParams, eneryMeterCtrl);
+    return httpUtils.get(url, headerParams, energyMeterCtrl);
   }
 }
