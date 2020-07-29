b2ba3fa667570780238727522513ca3cb44eac8b
王佳涛 <jiatao.wang@hekr.me>
2020-07-29 11:54:44 +0800

    baseResp示例

--- src/test/java/iot/example/iotcloudgateway/IotCloudGatewayApplicationTests.java
+++ src/test/java/iot/example/iotcloudgateway/IotCloudGatewayApplicationTests.java
@@ -4,20 +4,24 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import hekr.me.iotos.softgateway.common.dto.BaseResp;
 import hekr.me.iotos.softgateway.common.dto.TokenResp;
 import hekr.me.iotos.softgateway.utils.JsonUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IotCloudGatewayApplicationTests {
   public static void main(String[] args) {
-    BaseResp<TokenResp> tokenRespBaseResp = new BaseResp<>();
+    BaseResp<List<TokenResp>> tokenRespBaseResp = new BaseResp<>();
     tokenRespBaseResp.setInfo("请求(或处理)成功");
     tokenRespBaseResp.setStatusCode(200);
     TokenResp tokenResp = new TokenResp();
     tokenResp.setApplicationId("test");
     tokenResp.setId("c9648f504179bdd2");
     tokenResp.setKey("b62987f93ced7dd2");
-    tokenRespBaseResp.setData(tokenResp);
+    List<TokenResp> list = new ArrayList<>();
+    list.add(tokenResp);
+    tokenRespBaseResp.setData(list);
 
     String s = JsonUtil.toJson(tokenRespBaseResp);
-    BaseResp<TokenResp> o = JsonUtil.fromJson(s, new TypeReference<BaseResp<TokenResp>>() {});
+    BaseResp<List<TokenResp>> o = JsonUtil.fromJson(s, new TypeReference<BaseResp<List<TokenResp>>>() {});
     System.out.println(o);
   }
 }
