45d86abdbe0aad187b1924e9155ce94871e396bb
sdvdxl <sdvdxl@163.com>
2021-04-28 11:35:44 +0800

    fix action 映射
    Signed-off-by: sdvdxl <sdvdxl@163.com>

--- src/main/java/me/hekr/iotos/softgateway/common/enums/Action.java
+++ src/main/java/me/hekr/iotos/softgateway/common/enums/Action.java
@@ -7,12 +7,17 @@ import java.util.Map;
 import java.util.function.Function;
 import lombok.Getter;
 import me.hekr.iotos.softgateway.common.klink.AddTopo;
+import me.hekr.iotos.softgateway.common.klink.AddTopoResp;
 import me.hekr.iotos.softgateway.common.klink.BatchDevSend;
 import me.hekr.iotos.softgateway.common.klink.CloudSend;
 import me.hekr.iotos.softgateway.common.klink.DelTopo;
+import me.hekr.iotos.softgateway.common.klink.DelTopoResp;
 import me.hekr.iotos.softgateway.common.klink.DevLogin;
+import me.hekr.iotos.softgateway.common.klink.DevLoginResp;
 import me.hekr.iotos.softgateway.common.klink.DevLogout;
+import me.hekr.iotos.softgateway.common.klink.DevLogoutResp;
 import me.hekr.iotos.softgateway.common.klink.DevSend;
+import me.hekr.iotos.softgateway.common.klink.DevSendResp;
 import me.hekr.iotos.softgateway.common.klink.DevUpgrade;
 import me.hekr.iotos.softgateway.common.klink.GetConfig;
 import me.hekr.iotos.softgateway.common.klink.GetConfigResp;
@@ -29,21 +34,6 @@ public enum Action {
   /** 不支持 */
   NOT_SUPPORT("_notSupport_"),
 
-  /** 系统内部动作，mqtt 订阅事件 */
-  INNER_MQTT_SUB("innerMqttSub", FrameType.INNER),
-
-  /** 系统内部动作，mqtt 发布事件 */
-  INNER_MQTT_PUB("innerMqttPub", FrameType.INNER),
-
-  /** 网关上线，用于子设备推送 */
-  GATEWAY_LOGIN("gatewayLogin"),
-
-  /** 网关离线，用于子设备推送 */
-  GATEWAY_LOGOUT("gatewayLogout"),
-
-  /** 快照数据发生变化 */
-  DATA_CHANGED("dataChanged"),
-
   /**
    * app 发送
    *
@@ -54,32 +44,33 @@ public enum Action {
   CLOUD_SEND_RESP("cloudSendResp", FrameType.DEV_UP, KlinkResp.class),
 
   /** 心跳 */
-  HEARTBEAT("heartbeat", FrameType.DEV_UP, DevSend.class),
+  HEARTBEAT("heartbeat", FrameType.DEV_UP, Klink.class),
+  HEARTBEAT_RESP("heartbeatResp", FrameType.DEV_DOWN, KlinkResp.class),
 
   /** 设备发送 */
   DEV_SEND("devSend", FrameType.DEV_UP, DevSend.class),
   /** 设备发送结果回复 */
-  DEV_SEND_RESP("devSendResp", FrameType.DEV_DOWN),
+  DEV_SEND_RESP("devSendResp", FrameType.DEV_DOWN, DevSendResp.class),
 
   /** 设备批量发送 */
   BATCH_DEV_SEND("batchDevSend", FrameType.DEV_UP, BatchDevSend.class),
   /** 设备批量发送结果回复 */
-  BATCH_DEV_SEND_RESP("batchDevSendResp", FrameType.DEV_DOWN),
+  BATCH_DEV_SEND_RESP("batchDevSendResp", FrameType.DEV_DOWN, KlinkResp.class),
 
   /** 设备登录 */
   DEV_LOGIN("devLogin", FrameType.DEV_UP, DevLogin.class),
   /** 设备登录回复 */
-  DEV_LOGIN_RESP("devLoginResp", FrameType.DEV_DOWN),
+  DEV_LOGIN_RESP("devLoginResp", FrameType.DEV_DOWN, DevLoginResp.class),
 
   /** 设备登出 */
   DEV_LOGOUT("devLogout", FrameType.DEV_UP, DevLogout.class),
   /** 设备登出回复 */
-  DEV_LOGOUT_RESP("devLogoutResp", FrameType.DEV_DOWN),
+  DEV_LOGOUT_RESP("devLogoutResp", FrameType.DEV_DOWN, DevLogoutResp.class),
 
   /** 添加拓扑关系 */
   ADD_TOPO("addTopo", FrameType.DEV_UP, AddTopo.class),
   /** 添加拓扑关系回复 */
-  ADD_TOPO_RESP("addTopoResp", FrameType.DEV_DOWN),
+  ADD_TOPO_RESP("addTopoResp", FrameType.DEV_DOWN, AddTopoResp.class),
 
   /** 获取拓扑关系 */
   GET_TOPO("getTopo", FrameType.DEV_UP, GetTopo.class),
@@ -89,7 +80,7 @@ public enum Action {
   /** 添加拓扑关系 */
   DEL_TOPO("delTopo", FrameType.DEV_UP, DelTopo.class),
   /** 添加拓扑关系回复 */
-  DEL_TOPO_RESP("delTopoResp", FrameType.DEV_DOWN),
+  DEL_TOPO_RESP("delTopoResp", FrameType.DEV_DOWN, DelTopoResp.class),
 
   /** 注册设备 */
   REGISTER("register", FrameType.DEV_UP, Register.class),
