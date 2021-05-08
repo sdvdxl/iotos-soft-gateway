e23f5e2293d093883e3d1f8330a6c80407a265f0
sdvdxl <sdvdxl@163.com>
2021-05-08 19:08:50 +0800

    tcp client demo
    Signed-off-by: sdvdxl <sdvdxl@163.com>

--- example/src/main/java/me/hekr/iotos/softgateway/sample/TcpClientSample.java
+++ example/src/main/java/me/hekr/iotos/softgateway/sample/TcpClientSample.java
@@ -1,12 +1,40 @@
 package me.hekr.iotos.softgateway.sample;
 
+import cn.hutool.core.thread.ThreadUtil;
+import java.nio.charset.StandardCharsets;
+import me.hekr.iotos.softgateway.network.common.DecodePacket;
+import me.hekr.iotos.softgateway.network.common.PacketCoder;
+import me.hekr.iotos.softgateway.network.tcp.TcpClient;
+
 /**
  * tcp 客户端，
  *
  * @author iotos
  */
 public class TcpClientSample {
-  public static void main(String[] args) {
+  static PacketCoder<String> packetCoder =
+      new PacketCoder<String>() {
+        @Override
+        public byte[] encode(String s) {
+          return s.getBytes();
+        }
 
+        @Override
+        public DecodePacket decode(byte[] bytes) {
+          return DecodePacket.wrap(new String(bytes, StandardCharsets.UTF_8), bytes.length);
+        }
+      };
+
+  public static void main(String[] args) {
+    TcpClient<String> client = new TcpClient<>("localhost", 1024);
+    client.setSync(true);
+    client.setPacketCoder(packetCoder);
+    client.setTimeout(30000);
+    client.start();
+    for (int i = 0; i < 100; i++) {
+      String resp = client.send("hello");
+      System.out.println(resp);
+      ThreadUtil.sleep(1000);
+    }
   }
 }
