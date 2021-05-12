fb03c12b3c38c1e136b5b083f32c8ed5bacaf618
sdvdxl <sdvdxl@163.com>
2021-05-12 10:15:47 +0800

    2.0.0
    Signed-off-by: sdvdxl <sdvdxl@163.com>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+VERSION="2.0.0"
+mvn versions:set -Dversion=$VERSION
+mvn versions:update-child-modules
 
-mvn clean install deploy -DskipTests=true -DaltDeploymentRepository=hekr-maven::default::file:maven/repository/
+mvn  clean install deploy -DskipTests=true -DaltDeploymentRepository=hekr-maven::default::file:maven/repository/
