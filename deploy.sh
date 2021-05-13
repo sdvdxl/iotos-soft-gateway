c1156f9116550396703d29e1a217d34ae469399f
sdvdxl <sdvdxl@163.com>
2021-05-13 11:09:25 +0800

    update version to: 3.0.2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,42 @@
 #!/usr/bin/env bash
-VERSION="3.0.1-SNAPSHOT"
+# 使用 ./deploy install 安装到本地
+# ./deploy deploy 生成 maven jar包
+# ./deploy upload 生成 maven jar 包并 commit
+
+VERSION="3.0.2"
 echo "版本号：$VERSION"
-mvn versions:set -DnewVersion=$VERSION
-mvn versions:update-child-modules
 
-mvn  clean install -DskipTests=true
+updateVersion(){
+  mvn versions:set -DnewVersion=$VERSION versions:update-child-modules
+}
 
-rm -rf maven
-mvn deploy -DskipTests=true -DaltDeploymentRepository=hekr-maven::default::file:maven/repository/
+act=$1
 
-echo "发布位置：maven/repository"
+deploy() {
+  updateVersion
+  rm -rf maven
+  mvn deploy -DskipTests=true -DaltDeploymentRepository=hekr-maven::default::file:maven/repository/
+
+  echo "发布位置：maven/repository"
+}
+
+install() {
+  updateVersion
+  mvn clean install -DskipTests=true
+}
+
+upload(){
+  git add -A
+  git commit -m "update version to: $VERSION"
+  git push
+}
+
+if [[ "deploy" = "$act" ]] ; then
+  deploy
+elif [[ "install" = "$act" ]] ; then
+  install
+elif [[ "upload" = "$act" ]] ; then
+  upload
+else
+  echo 'deploy 或者 install'
+fi
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-VERSION="3.0.1-SNAPSHOT"
-echo "版本号：$VERSION"
-mvn versions:set -DnewVersion=$VERSION
-mvn versions:update-child-modules
-
-mvn  clean install -DskipTests=true
