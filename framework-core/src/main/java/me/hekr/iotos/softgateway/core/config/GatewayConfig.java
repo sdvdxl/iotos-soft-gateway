8a8ee9f9483d0ddd6c3fba314c1b6116a56f889c
sdvdxl <sdvdxl@163.com>
2021-05-24 14:23:25 +0800

    增加集群模式配置
    Signed-off-by: sdvdxl <sdvdxl@163.com>

--- framework-core/src/main/java/me/hekr/iotos/softgateway/core/config/GatewayConfig.java
+++ framework-core/src/main/java/me/hekr/iotos/softgateway/core/config/GatewayConfig.java
@@ -3,6 +3,7 @@ package me.hekr.iotos.softgateway.core.config;
 import cn.hutool.core.lang.Assert;
 import lombok.Getter;
 import lombok.ToString;
+import me.hekr.iotos.softgateway.core.enums.GatewayClusterMode;
 
 /** @author iotos */
 @Getter
@@ -11,7 +12,21 @@ public class GatewayConfig {
   protected String pk;
   protected String devId;
   protected String devSecret;
-  protected String clientId;
+  /**
+   * 集群模式
+   *
+   * <p>true 开启集群模式，即允许该网关（相同 pk，devId）同时登录多个客户端；都能首发消息； 构造的 clientId 一样
+   *
+   * <p>false 关闭集群模式，clientId 都是一样的；重复登录会相互顶替上下线，即只允许一个客户端在线
+   */
+  protected GatewayClusterMode clusterMode;
+
+  private String getTopicPrefix() {
+    if (clusterMode.isCluster()) {
+      return "$share/gateway/";
+    }
+    return "";
+  }
 
   public String getUpTopic() {
     assertValuesSet();
@@ -25,6 +40,6 @@ public class GatewayConfig {
 
   public String getDownTopic() {
     assertValuesSet();
-    return "down/dev/" + pk + "/" + devId;
+    return getTopicPrefix() + "down/dev/" + pk + "/" + devId;
   }
 }
--- framework-core/src/main/java/me/hekr/iotos/softgateway/core/config/IotOsConfig.java
+++ framework-core/src/main/java/me/hekr/iotos/softgateway/core/config/IotOsConfig.java
@@ -3,14 +3,17 @@ package me.hekr.iotos.softgateway.core.config;
 import javax.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import me.hekr.iotos.softgateway.common.utils.ParseUtil;
 import me.hekr.iotos.softgateway.core.constant.Constants;
+import me.hekr.iotos.softgateway.core.enums.GatewayClusterMode;
 import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /** @author iotos */
+@Slf4j
 @ComponentScan("me.hekr.iotos.softgateway.core")
 @Configuration
 public class IotOsConfig {
@@ -25,21 +28,34 @@ public class IotOsConfig {
   @Value("${connect.mqtt.connectionTimeout:10}")
   private int connectionTimeout;
 
+  /** 心跳保持，秒 */
   @Value("${connect.mqtt.keepAliveTime}")
   private int keepAliveTime;
 
-  @Value("${connect.mqtt.klink.queue.size:10}")
+  /** 发送的 klink queue 大小，默认1000，超过会阻塞 */
+  @Value("${connect.mqtt.klink.queue.size:1000}")
   @Getter
   private int klinkQueueSize;
 
+  /** 网关 pk */
   @Value("${gateway.pk}")
   private String gatewayPk;
 
+  /** 网关 devId */
   @Value("${gateway.devId}")
   private String gatewayDevId;
 
+  /** 网关设备的密钥 */
   @Value("${gateway.devSecret}")
   private String gatewayDevSecret;
+
+  /**
+   * 集群模式，默认单机模式
+   *
+   * @see GatewayConfig#
+   */
+  @Value("${gateway.cluster.mode:standalone}")
+  private String gatewayClusterMode;
 
   @PostConstruct
   public void init() {
@@ -47,16 +63,25 @@ public class IotOsConfig {
     gatewayConfig.pk = gatewayPk;
     gatewayConfig.devId = gatewayDevId;
     gatewayConfig.devSecret = gatewayDevSecret;
+    gatewayConfig.clusterMode = GatewayClusterMode.of(gatewayClusterMode);
 
     mqttConfig = new MqttConfig();
     MqttConfig mq = mqttConfig;
     GatewayConfig gw = gatewayConfig;
     mq.endpoint = endpoint;
     mq.clientId = "dev:" + gw.pk + ":" + gw.devId;
+    // 集群模式，clientId 要加 random，此处使用时间戳防止重复
+    if (gw.clusterMode != GatewayClusterMode.STANDALONE) {
+      mq.clientId += ":" + System.currentTimeMillis();
+    }
+
     mq.username = ParseUtil.HASH_METHOD + ":" + Constants.RANDOM;
     mq.password = getPassword(gw.pk, gw.devId, gw.devSecret, Constants.RANDOM).toCharArray();
     mq.connectTimeout = connectionTimeout;
     mq.keepAliveTime = keepAliveTime;
+
+    log.info("mqtt 配置: {}", mq);
+    log.info("网关配置：{}", gw);
   }
 
   @SneakyThrows
