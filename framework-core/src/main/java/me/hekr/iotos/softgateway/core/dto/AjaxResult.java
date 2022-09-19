bd37045676329a5756911f311526c26465a7a3ac
杜龙少 <sdvdxl@163.com>
2022-09-19 11:18:56 +0800

    分页帮助类
    Signed-off-by: 杜龙少 <sdvdxl@163.com>

---
+++ framework-core/src/main/java/me/hekr/iotos/softgateway/core/dto/AjaxResult.java
@@ -0,0 +1,129 @@
+package me.hekr.iotos.softgateway.core.dto;
+
+import cn.hutool.http.HttpStatus;
+import java.util.HashMap;
+
+/**
+ * 操作消息提醒
+ *
+ * @author ruoyi
+ */
+public class AjaxResult<T> extends HashMap<String, Object> {
+  /** 状态码 */
+  public static final String CODE_TAG = "code";
+  /** 返回内容 */
+  public static final String MSG_TAG = "message";
+  /** 数据对象 */
+  public static final String DATA_TAG = "data";
+
+  private static final long serialVersionUID = 1L;
+
+  /** 初始化一个新创建的 AjaxResult 对象，使其表示一个空消息。 */
+  public AjaxResult() {}
+
+  /**
+   * 初始化一个新创建的 AjaxResult 对象
+   *
+   * @param code 状态码
+   * @param msg 返回内容
+   */
+  public AjaxResult(int code, String msg) {
+    super.put(CODE_TAG, code);
+    super.put(MSG_TAG, msg);
+  }
+
+  /**
+   * 初始化一个新创建的 AjaxResult 对象
+   *
+   * @param code 状态码
+   * @param msg 返回内容
+   * @param data 数据对象
+   */
+  public AjaxResult(int code, String msg, T data) {
+    super.put(CODE_TAG, code);
+    super.put(MSG_TAG, msg);
+    if (data != null) {
+      super.put(DATA_TAG, data);
+    }
+  }
+
+  /**
+   * 返回成功消息
+   *
+   * @return 成功消息
+   */
+  public static <T> AjaxResult<T> success() {
+    return AjaxResult.success("操作成功");
+  }
+
+  /**
+   * 返回成功数据
+   *
+   * @return 成功消息
+   */
+  public static <T> AjaxResult<T> success(T data) {
+    return AjaxResult.success("操作成功", data);
+  }
+
+  /**
+   * 返回成功消息
+   *
+   * @param msg 返回内容
+   * @return 成功消息
+   */
+  public static <T> AjaxResult<T> success(String msg) {
+    return AjaxResult.success(msg, null);
+  }
+
+  /**
+   * 返回成功消息
+   *
+   * @param msg 返回内容
+   * @param data 数据对象
+   * @return 成功消息
+   */
+  public static <T> AjaxResult<T> success(String msg, T data) {
+    return new AjaxResult<>(0, msg, data);
+  }
+
+  /**
+   * 返回错误消息
+   *
+   * @return
+   */
+  public static <T> AjaxResult<T> error() {
+    return AjaxResult.error("操作失败");
+  }
+
+  /**
+   * 返回错误消息
+   *
+   * @param msg 返回内容
+   * @return 警告消息
+   */
+  public static <T> AjaxResult<T> error(String msg) {
+    return AjaxResult.error(msg, null);
+  }
+
+  /**
+   * 返回错误消息
+   *
+   * @param msg 返回内容
+   * @param data 数据对象
+   * @return 警告消息
+   */
+  public static <T> AjaxResult<T> error(String msg, T data) {
+    return new AjaxResult<>(HttpStatus.HTTP_BAD_REQUEST, msg, data);
+  }
+
+  /**
+   * 返回错误消息
+   *
+   * @param code 状态码
+   * @param msg 返回内容
+   * @return 警告消息
+   */
+  public static <T> AjaxResult<T> error(int code, String msg) {
+    return new AjaxResult<>(code, msg, null);
+  }
+}
---
+++ framework-network-http/src/main/java/me/hekr/iotos/softgateway/network/http/PageUtil.java
@@ -0,0 +1,79 @@
+package me.hekr.iotos.softgateway.network.http;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lombok.Data;
+
+/**
+ * @author du
+ */
+@Data
+public class PageUtil<T> implements Serializable {
+
+  private static final long serialVersionUID = 5926676981594352187L;
+  /** T 总条数 */
+  private long totalElements;
+
+  /** 当前页码 */
+  private int page;
+
+  /** 分页大小 */
+  private int size;
+  /** 内容 */
+  private List<T> content;
+
+  public static <T> PageUtil<T> of(int page, int size) {
+    PageUtil<T> pagec = new PageUtil<>();
+    pagec.page = page;
+    pagec.size = size;
+    return pagec;
+  }
+
+  public static <T> List<T> getAll(int initPage, int pageSize, PageCall<T> request) {
+    List<T> result = new ArrayList<>();
+    for (int i = initPage; ; i++) {
+      List<T> list = request.onPage(i, pageSize);
+      result.addAll(list);
+      if (list.size() < pageSize) {
+        break;
+      }
+    }
+    return result;
+  }
+
+  public static <T> void onPage(int initPage, int pageSize, PageCall<T> request) {
+    for (int i = initPage; ; i++) {
+      List<T> list = request.onPage(i, pageSize);
+      if (list.size() < pageSize) {
+        break;
+      }
+    }
+  }
+
+  /**
+   * 总页数
+   *
+   * @return 总页数
+   */
+  public int getTotalPages() {
+    return this.size == 0 ? 1 : (int) Math.ceil((double) totalElements / (double) this.size);
+  }
+
+  public List<T> getContent() {
+    return content == null ? Collections.emptyList() : content;
+  }
+
+  public interface PageCall<T> {
+
+    /**
+     * 分页进行的时候，自定义处理
+     *
+     * @param page 当前页码，从0开始
+     * @param size 分页大小
+     * @return List
+     */
+    List<T> onPage(int page, int size);
+  }
+}
